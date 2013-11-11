a861880f20458c0361e828fbc12cbc3f22ef1202
Markus Klein <m@klein.co.at>
2013-11-11 21:18:11 +0100

    stated implementing javascript functionallity

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,39 @@
+var slideShowContainer = $('#slideshow-container');
+ var picturePrefix = "./assets/img/";
+
+ var pictures = new Array(
+ "unsplash_5248748fb40ac_1.JPG",
+ "unsplash_5255bf45a4a45_1.JPG",
+ "unsplash_5249ec183ab2c_1.JPG",
+ "unsplash_5263607dd1bfc_1.JPG",
+ "unsplash_5252b10dacd20_1.JPG",
+ "unsplash_527bf5fd7f5f6_1.JPG"
+ );
+
+ function addFaceElement(pictureUrl, indexNum, pictureWidth){
+ slideShowContainer.children().append('<div class="face-wrapper">' +
+ '<div class="face" id="face-' + indexNum + '">' +
+ '<img src="' + pictureUrl + '">' +
+ '</div>' +
+ '<div id="face-' + indexNum + '-back"></div>' +
+ '</div>'
+ );
+
+ $('#face-' + indexNum).css('transform', 'rotateY(' + indexNum *  + ')')
 
 
-function sleep(milliseconds) {
-	var start = new Date().getTime();
-	for (var i = 0; i < 1e7; i++) {{
-		if ((new Date().getTime() - start) > milliseconds)
-			break;
-		}
-	}
-}
+ }
+
+ slideShowContainer.append('<div class="slideshow"></div>');
+ for(var i = 0; i < pictures.length; i++){
+
+ }
 
 var i = 0;
 
 function rotate(){
 	setTimeout(function(){
-		var transform = $(".polygone").css('transform')
+		var transform = $(".polygone").css('transform');
 		$(".polygone").css('transform', transform  + ' rotateY(-1deg)');
 		i++;
 		if(i < 60){
