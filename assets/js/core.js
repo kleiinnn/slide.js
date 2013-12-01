8a8f4ba6831680d5f70182de2b0545c18d4b2403
kleiinnn <m@mklein.co.at>
2013-12-01 15:05:06 +0100

    finished module implementation

--- assets/js/core.js
+++ assets/js/core.js
@@ -1,11 +1,37 @@
-var slideShowContainer = $('#slideshow-container');
-var picturePrefix = "./assets/img/";
-var radius = 600;
+var slideShowContainer;
+var i = 0;
 
-var pictures = new Array();
+exports.init = function(){
+	slideShowContainer = $('#' + config.slideShowContainerId);
+}
+
+exports.generateSlideshow = function(){
+	slideShowContainer.append('<div class="slideshow"></div>');
+	for(var i = 0; i < config.faces.length; i++){
+		addFaceElement(config.faces[i].picture, i+1, config.faces.length);
+	}
+	$('.slideshow').attr('style', ($('.slideshow').attr('style') == undefined ? "" : $('.slideshow').attr('style')) + ' width: ' + getFaceLength(config.faces.length) + 'px; height: ' + getFaceHeight(getFaceLength(config.faces.length)) + 'px;');
+	
+}
+
+
+
+exports.rotate = function(direction){
+	setTimeout(function(){
+		var transform = $(".slideshow").css('-webkit-transform');
+		$(".slideshow").css('-webkit-transform', transform  + ' rotateY(' + (direction != 'left' ? '-1' : '1') + 'deg)');
+		i++;
+		if(i < getAbsoluteRotationAngle(config.faces.length)){
+			exports.rotate(direction);
+		}
+		else{
+			i = 0;
+		}
+	}, 10);
+}
 
 function getFaceLength(pictureNum){
-	return Math.round(radius * 2 * Math.sin(Math.PI / pictureNum));
+	return Math.round(config.radius * 2 * Math.sin(Math.PI / pictureNum));
 }
 
 function getAbsoluteRotationAngle(pictureCount){
@@ -26,74 +52,16 @@ function getFaceHeight(length){
 
 function addFaceElement(pictureUrl, pictureNum, pictureCount){
 	console.log(pictureNum);
-	slideShowContainer.children().append('<div class="face-wrapper">' +
-		'<div style="transform: rotateY(' + getRotationAngle(pictureCount, pictureNum) + 'deg) translateZ(' + radius + 'px); width: ' + getFaceLength(pictureCount) + 'px; height: ' + getFaceHeight(pictureCount) + 'px;" class="face" id="face-' + pictureNum + '">' +
-		'<img src="' + picturePrefix + pictureUrl + '" style="width: ' + (getFaceLength(pictureCount) - 20) + 'px; height: ' + (getFaceHeight(pictureCount) - 20) + 'px;">' +
+	slideShowContainer.children().append(
+		'<div class="face-wrapper">' +
+		'<div style="-webkit-transform: rotateY(' + getRotationAngle(pictureCount, pictureNum) + 'deg) translateZ(' + config.radius + 'px); width: ' + getFaceLength(pictureCount) + 'px; height: ' + getFaceHeight(pictureCount) + 'px;" class="face" id="face-' + pictureNum + '">' +
+		'<img src="' + config.picturePrefix + pictureUrl + '" style="width: ' + (getFaceLength(pictureCount) - 20) + 'px; height: ' + (getFaceHeight(pictureCount) - 20) + 'px;">' +
 		'</div>' +
-		'<div class="face-back" style="background-color: rgb(' +  Math.floor(Math.random() * (256 + 1)) + ', ' + Math.floor(Math.random() * (256 + 1)) + ', ' + Math.floor(Math.random() * (256 + 1)) + '); transform: rotateY(' + getRotationAngle(pictureCount, pictureNum) + 'deg) translateZ(' + (radius - 1.2)  + 'px); width: ' + (getFaceLength(pictureCount) - 18) + 'px; height: ' + (getFaceHeight(pictureCount) - 18) + 'px;" id="face-' + pictureNum + '-back"></div>' +
+		'<div class="face-back" style="background-color: rgb(' +  Math.floor(Math.random() * (256 + 1)) + ', ' + Math.floor(Math.random() * (256 + 1)) + ', ' + Math.floor(Math.random() * (256 + 1)) + '); -webkit-transform: rotateY(' + getRotationAngle(pictureCount, pictureNum) + 'deg) translateZ(' + (config.radius - 1.2)  + 'px); width: ' + (getFaceLength(pictureCount) - 18) + 'px; height: ' + (getFaceHeight(pictureCount) - 18) + 'px;" id="face-' + pictureNum + '-back"></div>' +
 		'</div>'
 	);
 }
 
-slideShowContainer.append('<div class="slideshow"></div>');
-
-for(var i = 0; i < pictures.length; i++){
-	addFaceElement(pictures[i], i+1, pictures.length);
-}
-//add global css
-$('.slideshow').attr('style', ($('.slideshow').attr('style') == undefined ? "" : $('.slideshow').attr('style')) + ' width: ' + getFaceLength(pictures.length) + 'px; height: ' + getFaceHeight(getFaceLength(pictures.length)) + 'px;');
 
 
-
-
-var i = 0;
-
-function rotate(direction){
-	setTimeout(function(){
-		var transform = $(".slideshow").css('transform');
-		$(".slideshow").css('transform', transform  + ' rotateY(' + (direction != 'left' ? '-1' : '1') + 'deg)');
-		i++;
-		if(i < getAbsoluteRotationAngle(pictures.length)){
-			rotate(direction);
-		}
-		else{
-			i = 0;
-		}
-	}, 10)
-}
-
-var autoslide = null;
-var mousewheel = false;
-//setInterval(rotate, 2000);
-
-$('#right-control').click(function(){
-	rotate('left');
-});
-
-$('#left-control').click(function(){
-	rotate('right');
-});
-
-
-$('#autoslide').change(function(){
-	if(autoslide == null){
-		autoslide = setInterval(function(){rotate('left')}, 2000);
-	}
-	else{
-		clearInterval(autoslide);
-		autoslide = null;
-	}
-});
-
-$(window).bind('mousewheel', function(event) {
-	var transform = $(".slideshow").css('transform');
-	if(mousewheel){
-	    if (event.originalEvent.wheelDelta >= 0) {
-	        $(".slideshow").css('transform', transform  + ' rotateY(2deg)');
-	    }
-	    else {
-	        $(".slideshow").css('transform', transform  + ' rotateY(-2deg)');
-	    }
-	}   
-});
 
