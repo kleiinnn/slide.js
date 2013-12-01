62df56a393923939ac413c6fe5aa1d06723bfef3
kleiinnn <m@mklein.co.at>
2013-12-01 21:42:00 +0100

    commit

--- assets/js/audio.js
+++ assets/js/audio.js
@@ -1,14 +1,50 @@
-var context;
+var context, songBuffer;
+var source = null;
 
 exports.init = function(){
 	try{
-		context = new AudioContext();
+		context =  new webkitAudioContext();
 	}
 	catch(e){
-		alert("WebAudioAPI is not availible in you browser! Please use a browser which supports WebAudioAPI.");
+		alert("WebAudioAPI is not availible in you browser!");
 	}
 }
 
-exports.play = function(slideshowElement){
-	
+exports.play = function(elementId){
+	loadSong(config.directory + '/music/' + config.faces[elementId].sound, function(){
+		source = context.createBufferSource();
+		source.buffer = songBuffer;
+		//to be implemented
+		/*var processor = context.createScriptProcessor(8192);
+		processor.onaudioprocess = function(e){
+			console.log(e.timeStamp);
+		    var leftIn = e.inputBuffer.getChannelData(0);
+		    var rightIn = e.inputBuffer.getChannelData(1);
+		    var leftOut = e.outputBuffer.getChannelData(0);
+		    var rightOut = e.outputBuffer.getChannelData(1);
+			for(var i = 0; i < leftIn.length; i++){
+				leftOut[i] = leftIn[i];
+				rightOut[i] = rightIn[i];
+			}
+		}
+		source.connect(processor);
+		processor.connect(context.destination);*/
+		source.connect(context.destination);
+		source.start(0);
+	});
+}
+
+function loadSong(url, callback){
+	var request = new XMLHttpRequest();
+	request.open('GET', url, true);
+	request.responseType = 'arraybuffer';
+	request.onload = function(){
+		context.decodeAudioData(request.response, function(buffer){
+			songBuffer = buffer;
+			if(callback != null){
+				callback();
+			}
+		});
+	}
+	request.send();
 }
--- assets/js/core.js
+++ assets/js/core.js
@@ -14,6 +14,21 @@ exports.generateSlideshow = function(){
 	
 }
 
+var tmpArray = new Array(
+	"unsplash_5248748fb40ac_1.JPG",
+	"unsplash_5255bf45a4a45_1.JPG",
+	"unsplash_5249ec183ab2c_1.JPG",
+	"unsplash_5263607dd1bfc_1.JPG",
+	"unsplash_5252b10dacd20_1.JPG",
+	"unsplash_527bf5fd7f5f6_1.JPG",
+	"unsplash_5269924c8ce7c_1.JPG",
+	"unsplash_5252bb51404f8_1.JPG",
+	"unsplash_525a7e89953d1_1.JPG",
+	"unsplash_52509afaba7b1_1.JPG",
+	"unsplash_5263605581e32_1.JPG",
+	"unsplash_5261cd0183e57_1.JPG"
+);
+
 
 
 exports.rotate = function(direction){
@@ -51,13 +66,29 @@ function getFaceHeight(length){
 }
 
 function addFaceElement(pictureUrl, pictureNum, pictureCount){
-	console.log(pictureNum);
+	var appendString = '<div class="album">';
+	for(int i = 0; i < tmpArray.length; i++){
+		
+	}
 	slideShowContainer.children().append(
 		'<div class="face-wrapper">' +
 		'<div style="-webkit-transform: rotateY(' + getRotationAngle(pictureCount, pictureNum) + 'deg) translateZ(' + config.radius + 'px); width: ' + getFaceLength(pictureCount) + 'px; height: ' + getFaceHeight(pictureCount) + 'px;" class="face" id="face-' + pictureNum + '">' +
 		'<img src="' + config.picturePrefix + pictureUrl + '" style="width: ' + (getFaceLength(pictureCount) - 20) + 'px; height: ' + (getFaceHeight(pictureCount) - 20) + 'px;">' +
+		'<button class="audio-control" id="audio-control-' + pictureNum + '">Start</button>' +
 		'</div>' +
 		'<div class="face-back" style="background-color: rgb(' +  Math.floor(Math.random() * (256 + 1)) + ', ' + Math.floor(Math.random() * (256 + 1)) + ', ' + Math.floor(Math.random() * (256 + 1)) + '); -webkit-transform: rotateY(' + getRotationAngle(pictureCount, pictureNum) + 'deg) translateZ(' + (config.radius - 1.2)  + 'px); width: ' + (getFaceLength(pictureCount) - 18) + 'px; height: ' + (getFaceHeight(pictureCount) - 18) + 'px;" id="face-' + pictureNum + '-back"></div>' +
+		'</div>'
+	);
+}
+
+function addVerticalFaceElement(pictureUrl, pictureNum, pictureCount, container){
+	container.append(
+		'<div class="face-wrapper">' +
+		'<div style="-webkit-transform: rotateX(' + getRotationAngle(pictureCount, pictureNum) + 'deg) translateZ(' + config.radius + 'px); width: ' + getFaceLength(pictureCount) + 'px; height: ' + getFaceHeight(pictureCount) + 'px;" class="face" id="face-' + pictureNum + '">' +
+		'<img src="' + config.picturePrefix + pictureUrl + '" style="width: ' + (getFaceLength(pictureCount) - 20) + 'px; height: ' + (getFaceHeight(pictureCount) - 20) + 'px;">' +
+		'<button class="audio-control" id="audio-control-' + pictureNum + '">Start</button>' +
+		'</div>' +
+		'<div class="face-back" style="background-color: rgb(' +  Math.floor(Math.random() * (256 + 1)) + ', ' + Math.floor(Math.random() * (256 + 1)) + ', ' + Math.floor(Math.random() * (256 + 1)) + '); -webkit-transform: rotateX(' + getRotationAngle(pictureCount, pictureNum) + 'deg) translateZ(' + (config.radius - 1.2)  + 'px); width: ' + (getFaceLength(pictureCount) - 18) + 'px; height: ' + (getFaceHeight(pictureCount) - 18) + 'px;" id="face-' + pictureNum + '-back"></div>' +
 		'</div>'
 	);
 }
